a96a05e9137fcf057fbd33bd06bed8c1eaa3d382
Seth Kendall <sethpkendall@gmail.com>
2016-11-08 11:40:37 -0800

    properly finish the quiz and then transition to a random new route

--- app/components/movie-quiz.js
+++ app/components/movie-quiz.js
@@ -3,11 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   providedAnswer: null,
 
-  actions:{
-    submitAnswer(providedAnswer) {
-      if( movie.name = providedAnswer){
-
-      }
-    }
-  }
+  // actions:{
+  //   submitAnswer(providedAnswer) {
+  //     if( movie.name = providedAnswer){
+  //
+  //     }
+  //   }
+  // }
 });
--- app/routes/computer-quiz.js
+++ app/routes/computer-quiz.js
@@ -1,11 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  transition: Ember.inject.service(),
+
   model(){
     var url = "https://www.opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple";
     return Ember.$.getJSON(url).then(function(responseJSON){
       console.log(responseJSON.results);
       return responseJSON.results;
     });
+  },
+  actions: {
+    transitionToNew(){
+      var newRoute = this.get('transition').remove();
+      this.transitionTo(newRoute);
+      console.log(this.get('transition').categories)
+    }
   }
 });
