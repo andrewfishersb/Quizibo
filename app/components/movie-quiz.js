d773ddda814dc441a4ba436b20b7b29e13132a6f
Brian Dutz <Guest@epicodus-27.local>
2016-11-08 11:49:14 -0800

    update quiz functionality for movies and add answer key

--- app/components/movie-quiz.js
+++ app/components/movie-quiz.js
@@ -1,13 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  providedAnswer: null,
-
-  // actions:{
-  //   submitAnswer(providedAnswer) {
-  //     if( movie.name = providedAnswer){
-  //
-  //     }
-  //   }
-  // }
+  score: Ember.inject.service(),
+  quizzingNow: false,
+  currentQuestion: null,
+  counter: 0,
+  correctCounter: 0,
+  areResultsShowing: false,
+  actions:{
+    startQuiz(results){
+      this.set('quizzingNow', true);
+      this.set('currentQuestion', results.content[this.counter]._data.image);
+    },
+    nextQuestion(results){
+      var answer = this.get('answer');
+      this.set('answer', "");
+      answer = answer.toLowerCase();
+      console.log(answer);
+      if (answer === results.content[this.counter]._data.title) {
+        this.correctCounter++;
+        this.set('score.totalScore', this.correctCounter);
+      }
+      if(this.counter<10) {
+      this.counter++;
+      this.set('currentQuestion', results.content[this.counter]._data.image);
+    } else {
+        this.set('areResultsShowing', true)
+      }
+    }
+  }
 });
