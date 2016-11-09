1bfb38968dd5e52e13ee374cf9c007e22caca94f
Seth Kendall <Sethpkendall@gmail.com>
2016-11-09 14:36:03 -0800

    style presidents so they do not overlapp

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,6 +5,8 @@ export default Ember.Route.extend({
   transition: Ember.inject.service(),
   actions: {
     initialTransition(){
+      var newPlayer = this.store.createRecord('rental', params);
+      newRental.save();
       var newRoute = this.get('transition').remove();
       this.transitionTo(newRoute);
     }
