9e738d0d4509f1f287d2d657246308448520901c
Seth Kendall <Sethpkendall@gmail.com>
2016-11-09 11:51:06 -0800

    finish president quiz, image will appear when president if correct

--- app/components/president-quiz.js
+++ app/components/president-quiz.js
@@ -8,9 +8,6 @@ export default Ember.Component.extend({
     checkAnswer(results){
       var termArray='{"terms":[]}';
       var obj = JSON.parse(termArray);
-
-
-
       for (var i = 0; i < results.content.length; i++) {
         this.presidentArray.push(results.content[i]._data.name.toLowerCase());
         var curPresidentName = results.content[i]._data.name;
@@ -18,19 +15,24 @@ export default Ember.Component.extend({
         obj['terms'].push({"term":curPresidentTerm,"name":curPresidentName});
       }
       termArray=obj['terms'];
-      console.log(termArray);
-      var curGuess = this.get('guess');
+console.log(termArray);
+      var curGuess = this.get('guess').toLowerCase();
+      this.set('guess','');
       if(!this.guessedArray.includes(curGuess)){
         console.log('got here');
-        if (this.presidentArray.includes(curGuess.toLowerCase())) {
-          this.set('score.quizTotal', 1);
+        if (this.presidentArray.includes(curGuess)) {
+          var index= this.presidentArray.indexOf(curGuess);
+          if(curGuess==='grover cleveland'){
+            $('#' + termArray[index].term + '1').toggle();
+            $('#' + termArray[index].term + '2').show();
+            $('#' + termArray[index+2].term + '1').toggle();
+            $('#' + termArray[index+2].term + '2').show();
+          }
           this.guessedArray.push(curGuess);
+          $('#' + termArray[index].term + '1').toggle();
+          $('#' + termArray[index].term + '2').show();
+          this.set('score.quizTotal', 1);
           this.get('score').updateTotal();
-          console.log(this.guessedArray);
-          console.log('that is correct');
-          $('#2009-20171').toggle();
-          $('#2009-20172').show();
-          console.log();
         }
       }
 
